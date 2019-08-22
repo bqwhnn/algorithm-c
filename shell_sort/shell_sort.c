5d897ab31efba08afad3a886717d6f95134ab6c3
caiyangmin <18819457397@163.com>
2019-08-22 10:46:33 +0800

    希尔排序

---
+++ shell_sort/shell_sort.c
@@ -0,0 +1,10 @@
+void shell_sort(int a[], int n) {
+    int gap, i, j, temp;
+    for (gap = n/2; gap > 0; gap /= 2)
+        for (i = gap; i < n; i++)
+            for (j = i-gap; j >= 0 && a[j] > a[j+gap]; j -= gap) {
+                temp = a[j];
+                a[j] = a[j+gap];
+                a[j+gap] = temp;
+            }
+}
