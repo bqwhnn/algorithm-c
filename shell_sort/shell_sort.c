21c54966a5ec76cff72ebbb17e116f9c1d3f7e91
caiyangmin <18819457397@163.com>
2019-08-22 16:50:16 +0800

    希尔排序修改

--- shell_sort/shell_sort.c
+++ shell_sort/shell_sort.c
@@ -1,8 +1,11 @@
+#include "shell_sort.h"
+
 void shell_sort(int a[], int n) {
     int gap, i, j, temp;
+    
     for (gap = n/2; gap > 0; gap /= 2)
         for (i = gap; i < n; i++)
-            for (j = i-gap; j >= 0 && a[j] > a[j+gap]; j -= gap) {
+            for (j = i - gap; j >= 0 && a[j] > a[j+gap]; j -= gap) {
                 temp = a[j];
                 a[j] = a[j+gap];
                 a[j+gap] = temp;
